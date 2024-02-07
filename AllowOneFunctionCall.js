5ea4ca4b58f1f51f767226d7e9b538786bd505dc
Rasyid Annas <61853270+Rasyidannas@users.noreply.github.com>
2024-02-07 22:29:18 +0000

    AllowOneFunctionCall.js Added

---
+++ AllowOneFunctionCall.js
@@ -0,0 +1,25 @@
+// Allow One Function Call is only can call once and it will return undefined when cal more one time
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function (fn) {
+  let hasBeenCalled = false;
+  let result;
+
+  return function (...args) {
+    if (!hasBeenCalled) {
+      result = fn(...args);
+      hasBeenCalled = true;
+      return result;
+    }
+    return undefined;
+  };
+};
+
+let fn = (a, b, c) => a + b + c;
+let onceFn = once(fn);
+
+console.log(onceFn(1, 2, 3)); // 6
+console.log(onceFn(2, 3, 6)); // returns undefined without calling fn
